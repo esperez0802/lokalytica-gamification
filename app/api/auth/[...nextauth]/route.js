70860d2002d8991a7096afd074880121801eeefe
Emilio Perez <emilio.perez@obf.ateneo.edu>
2024-05-12 17:00:29 +0800

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -45,5 +45,6 @@ export const authOptions = {
 
 const handler = NextAuth(authOptions);
 
+export default NextAuth(authOptions);
 export const GET = (req, res) => handler(req, res);
 export const POST = (req, res) => handler(req, res);
